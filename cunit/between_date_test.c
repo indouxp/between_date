80bd4d8ee737cd9e8d70fcd1c4d696abf6cb7757
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2013-02-21 16:51:53 +0900

    add test case

--- cunit/between_date_test.c
+++ cunit/between_date_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <CUnit/CUnit.h>
 #include <CUnit/Console.h>
 #include "between_date.h"
@@ -8,6 +9,9 @@ void test_between_ymd_0010(void);
 void test_between_ymd_0020(void);
 void test_between_ymd_0030(void);
 void test_between_ymd_0040(void);
+void test_between_ymd_0050(void);
+void test_between_ymd_0060(void);
+void test_between_ymd_0070(void);
 
 int main(int argc, char *argv[])
 {
@@ -19,6 +23,9 @@ int main(int argc, char *argv[])
 	CU_add_test(between_ymd_suite, "test_0020", test_between_ymd_0020);
 	CU_add_test(between_ymd_suite, "test_0030", test_between_ymd_0030);
 	CU_add_test(between_ymd_suite, "test_0040", test_between_ymd_0040);
+	CU_add_test(between_ymd_suite, "test_0050", test_between_ymd_0050);
+	CU_add_test(between_ymd_suite, "test_0060", test_between_ymd_0060);
+	CU_add_test(between_ymd_suite, "test_0070", test_between_ymd_0070);
 
 	CU_console_run_tests();
 	CU_cleanup_registry();
@@ -47,4 +54,22 @@ void test_between_ymd_0040(void)
 {
 	int i_response = between_ymd("20130101", "20121231");
 	CU_ASSERT(i_response == -1);
+}
+
+void test_between_ymd_0050(void)
+{
+	int i_response = between_ymd("20121231", "20131231");
+	CU_ASSERT(i_response == 365);
+}
+
+void test_between_ymd_0060(void)
+{
+	int i_response = between_ymd("20131231", "20141231");
+	CU_ASSERT(i_response == 365);
+}
+
+void test_between_ymd_0070(void)
+{
+	int i_response = between_ymd("20111231", "20121231");
+	CU_ASSERT(i_response == 366);
 }
